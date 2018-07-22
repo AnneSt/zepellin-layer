c244fc2125e894dfa7ea6f819d8e81b386650f4b
AnneSt <38779675+AnneSt@users.noreply.github.com>
2018-07-22 23:24:06 +0200

    Update setup-environment.sh

--- setup-environment.sh
+++ setup-environment.sh
@@ -1,6 +1,11 @@
 LOCAL_CONF="conf/local.conf"
 DOWNLOADS_DIR="/opt/packagepool"
 SSTATE_CACHE_DIR="rpi/sstache"
+BUILD_DIR="rpi/build"
 
-export TEMPLATECONF="rpi/meta-zepellin/conf"
+export TEMPLATECONF="../rpi/meta-zepellin/conf"
 . poky-sumo/oe-init-build-env
+
+echo "DL_DIR = \"${DOWNLOADS_DIR}\"" >> $LOCAL_CONF
+echo "SSTATE_DIR = \"${SSTATE_CACHE_DIR}\"" >> $LOCAL_CONF
+echo "IMAGE_BUILD_NAME = \"${BUILD_DIR}\"" >> $LOCAL_CONF
